4c3a57e7cb7b19efc060096a6260fc65dcb98306
Yuanchun <immr.shen@gmail.com>
2020-05-03 01:03:21 +0800

    add mlp model

---
+++ models/mlp.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Mlp(nn.Module):
+    def __init__(self):
+        super(Mlp, self).__init__()
+        # number of hidden nodes in each layer (512)
+        hidden_1 = 512
+        hidden_2 = 512
+        # linear layer (784 -> hidden_1)
+        self.fc1 = nn.Linear(28 * 28, 512)
+        # linear layer (n_hidden -> hidden_2)
+        self.fc2 = nn.Linear(512, 512)
+        # linear layer (n_hidden -> 10)
+        self.fc3 = nn.Linear(512, 10)
+        # dropout layer (p=0.2)
+        # dropout prevents overfitting of data
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        # flatten image input
+        x = x.view(-1, 28 * 28)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc2(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add output layer
+        x = self.fc3(x)
+        return x
